e4240939de5eb2bcbab79d3796158ad747056fdd
ho dung <dungho2209@gmail.com>
2024-07-15 23:49:03 +0700

    upđate home and product detail

--- src/views/App.js
+++ src/views/App.js
@@ -1,28 +1,30 @@
 import React from "react";
-import '../styles/App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "../styles/App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import ADhome from "../views/admin/ADhome";
 import SanPham from "./user/SanPham";
 import NavHome from "./user/nav/NavHome";
 import Footer from "./user/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./user/Home";
+import ProductDetail from "./user/ProductDetail";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <BrowserRouter>
-                    <NavHome />
-                    <Routes>
-                        <Route path='/' element={<SanPham />} />
-                        <Route path='/ad' element={<ADhome />} />
-                    </Routes>
-                    <Footer />
-                </BrowserRouter>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <NavHome />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/ProductDetail" element={<ProductDetail />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/views/user/Home.js
@@ -0,0 +1,194 @@
+import React from "react";
+import "../../styles/user/Home.scss";
+import Image from "../../assets/images/sp1.png";
+import Image2 from "../../assets/images/sp2.png";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="container">
+          <h1 className="dung text-center pb-3">Sản phẩm mới</h1>
+          <div className="row">
+            <div className="col-md-2 product">
+              <img src={Image}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+          </div>
+          <div className="botton-xem">
+            <button align-item-center>Xem thêm</button>
+          </div>
+          <br></br>
+          <h1 className="dung text-center pb-3">Sản phẩm nổi bật</h1>
+          <div className="row">
+            <div className="col-md-2 product">
+              <img src={Image}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+            <div className="col-md-2 product">
+              <img src={Image2}></img>
+              <p>
+                Tượng hy lạp phong cách cổ đại bla bla bla
+                <br></br>
+                2.900.000đ
+              </p>
+            </div>
+          </div>
+          <div className="botton-xem">
+            <button align-item-center>Xem thêm</button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+//export {} MyComponent tra ra nhieu cai cung 1 luc
+export default Home;
