fe651ec37e1a4323c8272f79869c5f4fcf599022
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-07-21 02:08:36 +0700

    add layout admin

--- src/views/admin/ADhome.js
+++ src/views/admin/ADhome.js
@@ -5,40 +5,9 @@ import logo from '../../assets/images/shopdecor.png';
 class ADhome extends React.Component {
     render() {
         return (
-            <div className="web-body">
-                <div className="container-fluid admin-board">
-                    <div className="row">
-                        <div className="col-sm-2 menu-bar p-0">
-                            <div className="logo mb-3">
-                                <img src={logo} className="img-fluid" alt="Logo" />
-                            </div>
-                            <ul className="menu">
-                                <li className="menu-item active">Trang chủ</li>
-                                <li className="menu-item">Thống kê</li>
-                                <li className="menu-item">Đơn hàng</li>
-                                <li className="menu-item">Sản phẩm</li>
-                                <li className="menu-item">Người dùng</li>
-                                <li className="menu-item">Đăng xuất</li>
-                            </ul>
-                        </div>
-                        <div className="col-sm-10 ad-display p-0">
-                            <div className="ad-header">
-                                <ul className="nav-menu">
-                                    <li className="nav-menu-item dropdown">
-                                        <div className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        </div>
-                                        <ul className="dropdown-menu">
-                                            <li><a href="#" className="dropdown-item-ed" asp-area="">Đăng xuất</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="ad-content">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <>
+                <h1>đây là con</h1>
+            </>
         )
     }
 }
