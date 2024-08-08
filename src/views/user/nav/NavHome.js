9657e8d0c99da9ceb0f23711e2478f81fb6b34c5
Ro0190124 <128989515+Ro0190124@users.noreply.github.com>
2024-08-08 09:47:43 +0700

    Account

--- src/views/user/nav/NavHome.js
+++ src/views/user/nav/NavHome.js
@@ -11,6 +11,7 @@ import {
 import "../../../styles/user/nav/Nav.scss";
 import "../../../styles/user/btn/btn.scss";
 
+
 const NavHome = (props) => {
   const [isCartVisible, setIsCartVisible] = useState(false);
   const cartRef = useRef(null);
@@ -40,7 +41,8 @@ const NavHome = (props) => {
     event.stopPropagation();
     setListCart(cart => cart.filter((_, i) => i !== index));
   }
-
+ const userName = localStorage.getItem('userU');
+ console.log(userName);
   return (
     <>
       <nav className="navbar navbar-expand-sm">
@@ -127,7 +129,9 @@ const NavHome = (props) => {
                 </Link>
               </li>
               <li className="nav-item">
+                
                 <Link className="nav-link icon" to="/user">
+                 {userName}
                   <FontAwesomeIcon icon={faCircleUser} />
                 </Link>
               </li>
